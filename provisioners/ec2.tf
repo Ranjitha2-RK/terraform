e56b010a0eb992ec5b24c28a8f2826ac5d3c376e
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2026-01-26 16:33:07 +0530

    terraform

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -42,7 +42,7 @@ resource "aws_instance" "terraform" {
 }
 
 resource "aws_security_group" "allow_all_ports" {
-  name   = "allow-all-ports"
+  name = "allow-all-ports"
 
   egress {
     from_port        = 0 # from port 0 to to port 0 means all ports
